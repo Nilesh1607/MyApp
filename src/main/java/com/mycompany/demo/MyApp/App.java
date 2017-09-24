81de622e9e1e263e49985735d7a84dc91cb4d3f8
Nilesh Kumar <nilesh1607@gmail.com>
2017-09-24 13:25:28 +0530

    Practice for Test cases

--- src/main/java/com/mycompany/demo/MyApp/App.java
+++ src/main/java/com/mycompany/demo/MyApp/App.java
@@ -4,10 +4,38 @@ package com.mycompany.demo.MyApp;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	private String greeting;
+	
+	public String getGreeting() {
+			return this.greeting;
+	}
+	
+	public void setGreeting(String greet) {
+		if (isGreetingValid(greet)) {
+			this.greeting = greet;
+			}
+		}
+	
+	public boolean isGreetingValid(String greeting) {
+		if (greeting.contains("welcome")
+				|| greeting.contains("thanks")
+				|| greeting.contains("good afternoon")) {
+			return true;
+			
+		}
+		
+		return false;
+	}
+	
 }
+
+
+/**	
+*    public static void main( String[] args )
+*    {
+*        System.out.println( "Hello World!" );
+*    }
+*/
+	
+
